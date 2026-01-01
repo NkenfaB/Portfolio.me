92a1b4c5806cfae50a8458ca5399c2c6b8979cb7
Nkenfa Brandon <nkenfabrandon71@gmail.com>
2026-01-01 09:38:34 +0100

    Add missing disableTransitionOnChange prop to ThemeProvider

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -9,6 +9,7 @@ type ThemeProviderProps = {
   defaultTheme?: string;
   enableSystem?: boolean;
   storageKey?: string;
+  disableTransitionOnChange?: boolean;
 };
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
